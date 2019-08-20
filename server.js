454f08dc1b7bd5515327d8789cc8e70756a45c88
Nimblechapps <rajiv@nimblechapps.com>
2019-08-20 12:19:02 +0530

    files

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
 
-var port = process.env.PORT || 2013;
 
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(port);
+}).listen(2013);
 
 var io = require('socket.io').listen(app);
 
